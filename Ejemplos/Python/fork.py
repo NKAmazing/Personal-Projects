135ad3d89055623b0916a81af891f8693f465e76
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-04-07 01:40:16 -0300

    se agrega os exit a fork.py

--- Ejemplos/Python/fork.py
+++ Ejemplos/Python/fork.py
@@ -8,7 +8,7 @@ print('PID = %d , PPID = %d , Retorno: %d' %(os.getpid(), os.getppid(), retorno)
 
 if retorno == 0:
     print("este es el hijo")
-    # os._exit(7) # ejecuta un exit con estado terminacion: 7
+    os._exit(7) # ejecuta un exit con estado terminacion: 7
 else:
     print("este es el padre")
     ret = os.wait() # usa un wait para esperar al proceso hijo
