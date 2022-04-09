43345946121325b2af0c81a2b72e99e3a13845e3
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-04-09 19:59:38 -0300

    pid file to pid list with split

--- Ejemplos/Python/pid-dictionary.py
+++ Ejemplos/Python/pid-dictionary.py
@@ -1,16 +1,31 @@
 import os
+import time
 
 input_1 = int(input('Ingresar: '))
+input_2 = str(input("Ingresar archivo: "))
 
+fd = open(f"/home/nk-nicolas/Documentos/Apuntes/Personal-Projects/Ejemplos/Python/{input_2}.txt", "w+")
+fd.close
 PID_list = []
 for i in range(input_1):
     ret = os.fork()
-    pid = os.getpid()
-    PID_list.append(pid)
+    
     if ret == 0:
-        print(PID_list)
+        pid = os.getpid()
+        with open(f"/home/nk-nicolas/Documentos/Apuntes/Personal-Projects/Ejemplos/Python/{input_2}.txt", "a") as fd:
+            fd.write(str(pid) + ",")
         os._exit(0)
+
+time.sleep(1)
+with open(f"/home/nk-nicolas/Documentos/Apuntes/Personal-Projects/Ejemplos/Python/{input_2}.txt", "r") as fd:
+    lines = fd.read().split(",")
+    for line in lines:
+        print(line)
+        PID_list.append(line)
+    PID_list.remove("")
+    print(PID_list)
+     
 for i in range(input_1):
     os.wait()
-print(" soy el padre ")
+print("soy el padre")
 
