f4c4005c515404d364a4c7eef2a71c8b4249e4ec
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-04-13 11:13:24 -0300

    os pipe examples added

---
+++ Ejemplos/Python/os-pipe.py
@@ -0,0 +1,21 @@
+import os, time
+
+r, w = os.pipe() # creo un pipe con un archivo tipo lectura y escritura
+
+# si forkeo se comparte con el hijo, porque puedo acceder con el pipe
+ret = os.fork()
+
+if not ret:
+    os.write(w, b"hola mundo\n")
+    os._exit(0)
+
+time.sleep(1)
+os.wait()
+# espera a que termine el hijo y luego lo lee 
+leido = os.read(r, 100) # el 100 es la cantidad de digitos que lee de ese read
+
+print("Padre Leido: ", leido.decode())
+
+
+# si el hijo leyera el w vacio, quedaria en bucle
+# porque se quedaria esperando a que w tenga algo
---
+++ Ejemplos/Python/os-pipe2.py
@@ -0,0 +1,15 @@
+import os, time, sys
+
+r, w = os.pipe()
+
+ret = os.fork()
+
+if not ret:
+    while True:
+        leido = os.read(r, 100)
+        print("Hijo leido: ", leido.decode())
+    
+for i in sys.stdin:
+    os.write(w, bytes(i.encode()))
+
+
