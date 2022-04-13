aeeaf230c69dc50dc9f4ec5fcb6794711565c30f
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-04-13 11:14:13 -0300

    upper method added

--- Ejemplos/Python/os-pipe2.py
+++ Ejemplos/Python/os-pipe2.py
@@ -7,7 +7,7 @@ ret = os.fork()
 if not ret:
     while True:
         leido = os.read(r, 100)
-        print("Hijo leido: ", leido.decode())
+        print("Hijo leido: ", leido.decode().upper())
     
 for i in sys.stdin:
     os.write(w, bytes(i.encode()))
