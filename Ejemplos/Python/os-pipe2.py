9d5a9fb1d678171762ce6960d876afb18fc7fd18
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-04-20 10:19:54 -0300

    script and git ignore updated

--- Ejemplos/Python/os-pipe2.py
+++ Ejemplos/Python/os-pipe2.py
@@ -8,8 +8,6 @@ if not ret:
     while True:
         leido = os.read(r, 100)
         print("Hijo leido: ", leido.decode().upper())
-    
+
 for i in sys.stdin:
     os.write(w, bytes(i.encode()))
-
-
